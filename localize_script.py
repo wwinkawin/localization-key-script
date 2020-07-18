bf7167d825a37e29aff3ae11870dd312bcd1a26a
Kawin J <wwinkawin@gmail.com>
2020-07-18 21:37:34 +0700

    Split functions and enhance features

--- localize_script.py
+++ localize_script.py
@@ -25,100 +25,82 @@ def main():
             print ("Plase enter a version number")
     
 
-    try:
-        keys = pd.read_excel(file_name,sheet_name = ['FE Localization Keys','BE Status Codes'])
-    except Exception as e:
-        print('Unexpected error:' + str(e))
+    while True:
+        try:
+            keys = pd.read_excel(file_name,sheet_name = ['FE Localization Keys','BE Status Codes'])
+        except PermissionError:
+            decision = input('Please close file: ' + str(file_name) + '\nPress Enter')
+            if decision == "":
+                        continue
+        except Exception as e:
+            print('Unexpected error:' + str(e))
+        break
         
-    FE_processor(keys.get("FE Localization Keys"), fileVersion)
+    #Duplicate checking
+    keys_name = {
+        'FE Localization Keys' : 'String Name',
+        'BE Status Codes': 'Status Code'}
     
-    # #remove whitespace
-    # column_list = [0,1,2]
-    # for i in column_list:
-    #     keys.iloc[:,i] = keys.iloc[:,i].str.strip()
-    
-    # #Create isDuplucated check column
-    # isDuplicated = pd.DataFrame(data = keys.duplicated(['String Name'],keep=False),columns=['isDuplicated'])
+    duplicate_dict = duplicate_check(keys, keys_name)
+    FE_processor(keys.get("FE Localization Keys"), fileVersion, duplicate_dict.get("FE Localization Keys"))
     
     
-    # #Concat isDuplucated check column
-    # keys = pd.concat([keys,isDuplicated],axis=1)
+def duplicate_check(keys,keys_name):
     
-    # while True:
-    #     try:
-    #         keys.to_excel('Localization_key_isDuplicated.xlsx', sheet_name = sheet_name,index = False)
-    #     except xlsxwriter.exceptions.FileCreateError as e:
-    #         decision = input("Cannot write to Excel file.\nPlease close "
-    #                          "'Localization_key_isDuplicated.xlsx'\n"
-    #                          "Press Enter")
-    #         if decision == "":
-    #             continue
-    #     break
+    df_dict = {}
+    duplicate_dict = {}
+    
+    #Get BE and FE sheets
+    for sheet_name, column_name in keys_name.items():        
+        sheet = keys.get(sheet_name)
+        
+        #Create isDuplucated column
+        isDuplicated = pd.DataFrame(data = sheet.duplicated([column_name],keep=False),columns=['isDuplicated'])
+        
+        duplicate_dict[sheet_name] = isDuplicated
+        
+        #check if there are any duplicated rows
+        if (isDuplicated.values.sum() != 0):
+            #If there is, append dict with key = sheet_name, values = dataframe
+            df_dict[sheet_name] = pd.concat([sheet,isDuplicated],axis=1)
 
-    # #Get Current DateTime
-    
-    # currentTime = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S+07:00"))
-    
-    # #Remove duplicated rows
-    # keys = keys[keys['isDuplicated'] == False]
-    
-    
-    # #Split dataframe
-    # en = keys.iloc[:,[0,1]].set_index('String Name')
-    # en.rename(columns={en.columns[0]:'en'},inplace=True)
-    
-    # th = keys.iloc[:,[0,2]].set_index('String Name')
-    # th.rename(columns={th.columns[0]:'th'},inplace=True)
-    
-    # #Convert dataframe to dictionaries
-    # en_dict = en.to_dict()
-    # th_dict = th.to_dict()
-    
-    # #Combine dictionaries
-    # data = {
-    #     "versionNumber":fileVersion,
-    #     "languagePackLastModified":currentTime,
-    #     "content":{**en_dict, **th_dict}
-    # }
-    
-    
-    # #Write JSON file
-    
-    # with open('data.json', 'w', encoding='utf-8') as f:
-    #     json.dump(data, f, ensure_ascii=False, indent=4)
+    #Create excel file if there are duplicated rows
+    if df_dict:
+        while True:
+            try:
+                with pd.ExcelWriter('Localization_key_isDuplicated.xlsx') as writer:
+                    for sheet_name, df in df_dict.items():
+                        df.to_excel(writer, sheet_name=sheet_name, index = False)
+                        
+            except xlsxwriter.exceptions.FileCreateError:
+                decision = input("Cannot write to Excel file.\nPlease close "
+                                  "'Localization_key_isDuplicated.xlsx'\n"
+                                  "Press Enter")
+                if decision == "":
+                    continue
+            break
+    return duplicate_dict
 
-def FE_processor(keys, fileVersion):
+def BE_processor(keys):
+    column_list = list(range(6))
+    for i in column_list:
+        keys.iloc[:,i] = keys.iloc[:,i].str.strip()
+        
+    #Create isDuplucated check column
+    isDuplicated = pd.DataFrame(data = keys.duplicated(['Status Code'],keep=False),columns=['isDuplicated'])
+        
+        
+def FE_processor(keys, fileVersion, isDuplicated):
     #remove whitespace
     column_list = [0,1,2]
     for i in column_list:
         keys.iloc[:,i] = keys.iloc[:,i].str.strip()
-    
-    #Create isDuplucated check column
-    isDuplicated = pd.DataFrame(data = keys.duplicated(['String Name'],keep=False),columns=['isDuplicated'])
-    
-    
-    if (isDuplicated.sum() != 0):
-        
-        #Concat isDuplucated check column
-        keys = pd.concat([keys,isDuplicated],axis=1)
-        
-        #Write isDuplicated excel file
-        while True:
-            try:
-                keys.to_excel('Localization_key_isDuplicated.xlsx', sheet_name = 'FE Localization Keys',index = False)
-            except xlsxwriter.exceptions.FileCreateError:
-                decision = input("Cannot write to Excel file.\nPlease close "
-                                 "'Localization_key_isDuplicated.xlsx'\n"
-                                 "Press Enter")
-                if decision == "":
-                    continue
-            break
 
     #Get Current DateTime in specified format
     currentTime = str(datetime.now().strftime("%Y-%m-%dT%H:%M:%S+07:00"))
     
     #Remove duplicated rows
-    keys = keys[keys['isDuplicated'] == False]
+    keys = keys[isDuplicated.values == False]
     
     #Split dataframe
     en = keys.iloc[:,[0,1]].set_index('String Name')
@@ -138,9 +120,15 @@ def FE_processor(keys, fileVersion):
         "content":{**en_dict, **th_dict}
     }
     
-    #Write JSON file
-    fileName = 'language_pack_' + str(datetime.now().strftime("%Y%m%d")) + "_1700_v" + str(fileVersion) + ".json"
-    with open(fileName, 'w', encoding='utf-8') as f:
+    #Gen file name
+    file_name = 'language_pack_' + str(datetime.now().strftime("%Y%m%d")) + "_1700_v" + str(fileVersion) + ".json"
+    
+    #Wrtie JSON file
+    json_writer(data, file_name)
+    
+
+def json_writer(data,file_name):
+    with open(file_name, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
     
 if __name__ == '__main__':
